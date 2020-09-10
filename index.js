6f1ab5be30db792343fd5e21b9bb5d311013ba2d
Daniel Cabrera <dcabrerabad98@gmail.com>
2020-09-09 19:04:44 -0500

    interfaz de busqueda de pokemon terminada, error de scroll solucionado

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const ONBUTTON = document.querySelector('.button')
+const ONBUTTON = document.querySelector('#btnEncender')
 const PANTALLA = document.getElementById('pantalla')
 var BASEURL =  `https://pokeapi.co/api/v2/pokemon/?limit=30`
 var nextURL
 const search = document.getElementById('search')
+const OFFBUTTON = document.querySelector('#btnApagar')
+
+OFFBUTTON.style.display = 'none'
 toggleScreen()
 
 function toggleScreen(){
@@ -10,10 +13,16 @@ function toggleScreen(){
         PANTALLA.style.backgroundColor = '#CFD6C6'
         pantallaPokemons()
         renderPokemons(BASEURL)
+        ONBUTTON.style.display = 'none'
+        OFFBUTTON.style.display = 'block'
         search.disabled = false
     }else{
         PANTALLA.style.backgroundColor = 'black'
+        ONBUTTON.style.display = 'block'
+        OFFBUTTON.style.display = 'none'
+        nextURL = ''
         PANTALLA.innerHTML = ''
+        search.value = ''
         search.disabled = true
     }
 }
@@ -51,10 +60,9 @@ async function renderPokemons(url){
     return data.next
 }
 
-
 PANTALLA.addEventListener('scroll', () => {
-    const scrollable = PANTALLA.scrollHeight - PANTALLA.scrollTop
-    const scroller = PANTALLA.clientHeight
+const scrollable = PANTALLA.scrollHeight - PANTALLA.scrollTop
+const scroller = PANTALLA.clientHeight
     if(Math.ceil(scrollable) === scroller){
         morePokemons()
     }
@@ -81,6 +89,7 @@ function pantallaPokemon(){
 function btnBack() {
     limpiarPantalla()
     pantallaPokemons()
+    nextURL = ''
     renderPokemons(BASEURL)
     search.value = null
 } 
@@ -90,17 +99,23 @@ async function morePokemons(){
 }
 
 search.addEventListener('change', () => {
-    limpiarPantalla()
-    pantallaPokemon()
-    renderPokemon(search.value)
+    if(search.value === ''){
+        window.alert('Inserta un id de pokemon')
+    }else{
+        limpiarPantalla()
+        pantallaPokemon()
+        renderPokemon(search.value)
+    }
 })
 
 async function renderPokemon(ID){
+    PANTALLA.removeEventListener('scroll', null)
     const data = await getPokemon(ID)
     const container = document.createElement('div')
     container.className = 'pkmnContainer'
     container.innerHTML = `
-    <div class='pkmn-name'>${data.name}</div>
+    <div class='pkmn-name'>
+    <p>${data.name}</p></div>
     <img src='${data.sprites.front_default}'">
     <div class='itemContainer'>Item</div>
     <div class='whiteSeparator'>None</div>
@@ -108,9 +123,40 @@ async function renderPokemon(ID){
     const infoContainer = document.createElement('div')
     infoContainer.className = 'infoContainer'
     infoContainer.innerHTML = `
-    <p>Pokédex No. ${data.id}</p>
-    <p>Name ${data.name}</p>
-    <p>Type ${data.types[0].type.name}</p>`
+    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+        <li class="nav-item" role="presentation">
+            <a class="nav-link active" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="home" aria-selected="true" style='background-color: #85DE8C'>I</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="nav-link" id="movements-tab" data-toggle="tab" href="#movements" role="tab" aria-controls="movements" aria-selected="false" style='background-color: #FB9E96'>M</a>
+        </li>
+        <li class="nav-item" role="presentation">
+            <a class="nav-link" id="forms-tab" data-toggle="tab" href="#forms" role="tab" aria-controls="forms" aria-selected="false" style='background-color: #8895F8;'>F</a>
+        </li>
+    </ul>
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="info" role="tabpanel" aria-labelledby="info-tab" style='background-color: #85DE8C'>
+            <p style='margin: auto; width: 40px'>Info</p>
+            <p>Pokédex No. ${data.id}</p>
+            <p>Name ${data.name}</p>
+            <p>Type ${data.types[0].type.name}</p>
+        </div>
+        <div class="tab-pane fade" id="movements" role="tabpanel" aria-labelledby="movements-tab" style='background-color: #FB9E96'>
+            <p style='margin: auto; width: 110px'>Movements</p>
+            <p>${data.moves[0].move.name}</p>
+            <p>${data.moves[1].move.name}</p>
+            <p>${data.moves[2].move.name}</p>
+        </div>
+        <div class="tab-pane fade overflow-auto" id="forms" role="tabpanel" aria-labelledby="forms-tab" style='background-color: #8895F8; height: 175px'>
+            <p style='margin: auto; width: 123px'>Default Form</p>
+            <img src='${data.sprites.back_default}' class="rounded mx-auto" style='margin: auto;'> 
+            <img src='${data.sprites.front_default}' class="rounded mx-auto" style='margin: auto;'> 
+            <p style='margin: auto; width: 123px'>Shiny Form</p>
+            <img src='${data.sprites.back_shiny}' class="rounded mx-auto" style='margin: auto;'> 
+            <img src='${data.sprites.front_shiny}' class="rounded mx-auto" style='margin: auto;'> 
+        </div>
+    </div>
+    `
     
     const pkmnHeader = document.createElement('div')
     pkmnHeader.className = 'pkmnHeader'
