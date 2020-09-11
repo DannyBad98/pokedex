278aade633aec0b66e215b5434b70026f39a27e5
Daniel Cabrera <dcabrerabad98@gmail.com>
2020-09-11 14:34:14 -0500

    Validacion de input agregada[C

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ const scrollable = PANTALLA.scrollHeight - PANTALLA.clientHeight
 const scroller = PANTALLA.scrollTop
 const screll = scroller / scrollable
     if(screll >= 0.98){
-        console.log(`scrollable ${scrollable} scroller ${scroller} ${screll}`)
         morePokemons()
     }
 })
@@ -100,71 +99,77 @@ async function morePokemons(){
 
 search.addEventListener('change', () => {
     if(search.value === ''){
-        window.alert('Inserta un id de pokemon')
+        window.alert('Inserta un id o nombre valido de pokemon')
     }else{
         limpiarPantalla()
         pantallaPokemon()
-        renderPokemon(search.value)
+        getPokemon(search.value.toLowerCase()).then(renderPokemon(search.value.toLowerCase())).catch(onError)
     }
 })
+
+function onError(){
+    window.alert('Inserta un id o nombre valido de pokemon')
+    pantallaPokemons()
+    renderPokemons(BASEURL)
+}
 
 async function renderPokemon(ID){
     PANTALLA.removeEventListener('scroll', null)
     const data = await getPokemon(ID)
-    const container = document.createElement('div')
-    container.className = 'pkmnContainer'
-    container.innerHTML = `
-    <div class='pkmn-name'>
-    <p>${data.name}</p></div>
-    <img src='${data.sprites.front_default}'">
-    <div class='itemContainer'>Item</div>
-    <div class='whiteSeparator'>None</div>
-    `
-    const infoContainer = document.createElement('div')
-    infoContainer.className = 'infoContainer'
-    infoContainer.innerHTML = `
-    <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+        const container = document.createElement('div')
+        container.className = 'pkmnContainer'
+        container.innerHTML = `
+        <div class='pkmn-name'>
+        <p>${data.name}</p></div>
+        <img src='${data.sprites.front_default}'">
+        <div class='itemContainer'>Item</div>
+        <div class='whiteSeparator'>None</div>
+        `
+        const infoContainer = document.createElement('div')
+        infoContainer.className = 'infoContainer'
+        infoContainer.innerHTML = `
+        <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
         <li class="nav-item" role="presentation">
-            <a class="nav-link active" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="home" aria-selected="true" style='background-color: #85DE8C'>I</a>
+        <a class="nav-link active" id="info-tab" data-toggle="tab" href="#info" role="tab" aria-controls="home" aria-selected="true" style='background-color: #85DE8C'>I</a>
         </li>
         <li class="nav-item" role="presentation">
-            <a class="nav-link" id="movements-tab" data-toggle="tab" href="#movements" role="tab" aria-controls="movements" aria-selected="false" style='background-color: #FB9E96'>M</a>
+        <a class="nav-link" id="movements-tab" data-toggle="tab" href="#movements" role="tab" aria-controls="movements" aria-selected="false" style='background-color: #FB9E96'>M</a>
         </li>
         <li class="nav-item" role="presentation">
-            <a class="nav-link" id="forms-tab" data-toggle="tab" href="#forms" role="tab" aria-controls="forms" aria-selected="false" style='background-color: #8895F8;'>F</a>
+        <a class="nav-link" id="forms-tab" data-toggle="tab" href="#forms" role="tab" aria-controls="forms" aria-selected="false" style='background-color: #8895F8;'>F</a>
         </li>
-    </ul>
-    <div class="tab-content" id="myTabContent">
+        </ul>
+        <div class="tab-content" id="myTabContent">
         <div class="tab-pane fade show active" id="info" role="tabpanel" aria-labelledby="info-tab" style='background-color: #85DE8C'>
-            <p style='margin: auto; width: 40px'>Info</p>
-            <p>Pokédex No. ${data.id}</p>
-            <p>Name ${data.name}</p>
-            <p>Type ${data.types[0].type.name}</p>
+        <p style='margin: auto; width: 40px'>Info</p>
+        <p>Pokédex No. ${data.id}</p>
+        <p>Name ${data.name}</p>
+        <p>Type ${data.types[0].type.name}</p>
         </div>
         <div class="tab-pane fade" id="movements" role="tabpanel" aria-labelledby="movements-tab" style='background-color: #FB9E96'>
-            <p style='margin: auto; width: 110px'>Movements</p>
-            <p>${data.moves[0].move.name}</p>
-            <p>${data.moves[1].move.name}</p>
-            <p>${data.moves[2].move.name}</p>
+        <p style='margin: auto; width: 110px'>Movements</p>
+        <p>${data.moves[0].move.name}</p>
+        <p>${data.moves[1].move.name}</p>
+        <p>${data.moves[2].move.name}</p>
         </div>
         <div class="tab-pane fade overflow-auto" id="forms" role="tabpanel" aria-labelledby="forms-tab" style='background-color: #8895F8; height: 175px'>
-            <p style='margin: auto; width: 123px'>Default Form</p>
-            <img src='${data.sprites.back_default}' class="rounded mx-auto" style='margin: auto;'> 
-            <img src='${data.sprites.front_default}' class="rounded mx-auto" style='margin: auto;'> 
-            <p style='margin: auto; width: 123px'>Shiny Form</p>
-            <img src='${data.sprites.back_shiny}' class="rounded mx-auto" style='margin: auto;'> 
-            <img src='${data.sprites.front_shiny}' class="rounded mx-auto" style='margin: auto;'> 
+        <p style='margin: auto; width: 123px'>Default Form</p>
+        <img src='${data.sprites.back_default}' class="rounded mx-auto" style='margin: auto;'> 
+        <img src='${data.sprites.front_default}' class="rounded mx-auto" style='margin: auto;'> 
+        <p style='margin: auto; width: 123px'>Shiny Form</p>
+        <img src='${data.sprites.back_shiny}' class="rounded mx-auto" style='margin: auto;'> 
+        <img src='${data.sprites.front_shiny}' class="rounded mx-auto" style='margin: auto;'> 
         </div>
-    </div>
-    `
-    
-    const pkmnHeader = document.createElement('div')
-    pkmnHeader.className = 'pkmnHeader'
-    pkmnHeader.innerHTML = `
-    <img src='./img/return.png' width='30px' onclick="btnBack()">
-    POKéMON INFO`
-
-    PANTALLA.appendChild(pkmnHeader)
-    PANTALLA.appendChild(container)
-    PANTALLA.appendChild(infoContainer)
+        </div>
+        `
+        
+        const pkmnHeader = document.createElement('div')
+        pkmnHeader.className = 'pkmnHeader'
+        pkmnHeader.innerHTML = `
+        <img src='./img/return.png' width='30px' onclick="btnBack()">
+        POKéMON INFO`
+        
+        PANTALLA.appendChild(pkmnHeader)
+        PANTALLA.appendChild(container)
+        PANTALLA.appendChild(infoContainer)
 }
